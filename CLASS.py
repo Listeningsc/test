9fadf42605efd4cbc2944680b18f0638b792f8cc
Listeningsc <listening@whu.edu.cn>
2022-03-23 11:28:49 +0800

    there are 2 files updated

---
+++ CLASS.py
@@ -0,0 +1,23 @@
+class Dog():
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def run(self):
+        print('is or not auto')
+
+    def rol(self):
+        print(self.name + 'PLEASE sit')
+
+    def sit(self):
+        print(self.age + '123')
+
+
+
+mydog = Dog('hashqi',8)
+mydog.run()
+
+
+
+
+
